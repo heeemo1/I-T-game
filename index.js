0d8a6863e675112109e5ebe99cf01fb826932f5d
heeemo1 <hakeerg000@gmail.com>
2025-07-10 18:08:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -158,3 +158,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`✅ السيرفر يعمل على http://localhost:${PORT}`);
 });
+server.listen(PORT, () => {
+  console.log(`✅ السيرفر يعمل على المنفذ ${PORT}`);
+});
